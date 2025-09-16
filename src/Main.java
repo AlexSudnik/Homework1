b1becb1e1c4ef7ba8e81c594b8ea7be6a83efb08
AlexSudnik <b.f.pierce@icloud.com>
2025-09-16 10:52:31 +0300

    task 1 of 3 completed

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        Task1 vc= new Task1();
+        vc.glasOrSoglasn();
+
+        Task1_2 vc1= new Task1_2();
+        vc1.glasOrSoglasn1();
+
         }
     }
-}
+
+
+
+
