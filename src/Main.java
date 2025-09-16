e72e0ea69306a7680c96ecbf9a0ab31808666992
AlexSudnik <b.f.pierce@icloud.com>
2025-09-16 20:35:38 +0300

    task 3 of 4 completed

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,14 @@ public class Main {
         Task1_2 vc1= new Task1_2();
         vc1.glasOrSoglasn1();
 
+        Task2 vc3= new Task2();
+        vc3.monthSelect();
+
+        Task3 vc4= new Task3();
+        vc4.exitTask();
+
+        Task4 vc5= new Task4();
+        vc5.devideOn3();
         }
     }
 
